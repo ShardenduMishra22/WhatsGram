e12994518ae336c27213a1029ff76e4c891e8ccc
Shardendu Mishra <shardendumishra01@gmail.com>
2024-09-17 20:38:30 +0530

    Footer component added

--- frontend/whatsgram/src/App.jsx
+++ frontend/whatsgram/src/App.jsx
@@ -1,7 +1,11 @@
+import Footer from "./Footer.jsx"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+        <footer>
+            <Footer />
+        </footer>
+    </>
   )
 }
