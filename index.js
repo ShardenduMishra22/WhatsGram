bc5809334398b0594e622e4782a8f568f5a97958
Shardendu Mishra <shardendumishra01@gmail.com>
2024-09-20 19:14:41 +0530

    Updated

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ import userRouter from './rout/userRout.js';
 import cookieParser from "cookie-parser";
 import path from "path";
 import { app, server } from './Socket/socket.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
@@ -22,7 +25,7 @@ app.use('/api/message', messageRouter);
 app.use('/api/user', userRouter);
 
 // Serve static front-end from 'dist'
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Serve front-end for all other routes
 app.get("*", (req, res) => {
